c212401b37cb1e9c8971a623000a0857ce6ff147
Si-Min1 <rlaxogh119@naver.com>
2022-01-14 17:09:33 +0900

    파이썬_1일차_진행

---
+++ Day_1/Basic_syntax01.py
@@ -0,0 +1,40 @@
+# 주석
+'''
+# 문자열 표현 방법
+print('hello World')
+print("hello, World")
+
+print(3+2)
+
+
+
+# 변수 사용 
+a = "asd"
+b = "qwer"
+
+print(a,b)
+print(a + b)
+
+
+
+# 연산
+print(3+2)
+print(3-2)
+print(3+2)
+print(7/2)
+print(7//2)     # 정수값만
+print(26%7)     # 나머지
+print(10**2)    # 제곱
+
+
+
+# 형변환
+q= '10'
+print(int(q)+2) #됨
+# print (q+2)    안됨
+
+
+# 타입 확인
+print(type(10))
+print(type(10.0))
+print(type("10"))       '''
